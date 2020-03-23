7aa15812c10202174554f0143c0dd819e064a685
Nevets01 <62570039+Nevets01@users.noreply.github.com>
2020-03-23 16:37:07 -0400

    Update zcommand.ino

--- zimodem/zcommand.ino
+++ zimodem/zcommand.ino
@@ -1,5 +1,6 @@
 /*
    Copyright 2016-2019 Bo Zimmerman
+   Modified 2020 by Nevets01
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
